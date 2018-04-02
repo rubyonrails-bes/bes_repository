18dbf7ae8e7ab74c8e4ae5bb6d49677dbd9e2bc2
avinash <kumarabavinash09@gmail.com>
2018-04-02 15:43:52 +0530

    pp

--- bes_app/config/routes.rb
+++ bes_app/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+get 'user/create'
+get 'user/new'
+root "user#new"
 end
