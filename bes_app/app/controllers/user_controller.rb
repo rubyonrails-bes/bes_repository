ce31181cb3f338150df92ef955c72753ed98488d
roopesh <roopesh.v1595@gmail.com>
2018-04-03 14:25:07 +0530

    user commit

--- bes_app/app/controllers/user_controller.rb
+++ bes_app/app/controllers/user_controller.rb
@@ -1,29 +1,21 @@
 class UserController < ApplicationController
 def new
 	@besapp = User.all
-	#raise params.inspect
 end
 
   def create
-  	#raise VisitorBesolution.inspect
   	raise params.inspect
   	@besapp = User.new(bes_params)
-  	  	#@visitor = VisitorBesolution.new(:name => params[:visitor_besolution][:name]) rescue nil
-  	  	#@visitor = VisitorBesolution.new(params[:visitor_besolution]) rescue nil
-         #@visitor.name = params[:visitor_besolution][:name] rescue nil
-#raise @visitor.name.inspect
-    #@product.user = current_user
+  	  	
     if @besapp.save
-      #gggg
+      
       redirect_to :visitor_create_url, notice: 'Visitor was successfully saved.'
-          #flash[:notice] = "Form Created and Saved Successfully."
     else
       render :show
     end
   end
   def show
     hhhhh
-   # @visitor = VisitorBesolution.all
        @visitor = User.find(params[:id])
 
   end
@@ -35,7 +27,6 @@ end
   end
 
   def bes_params
-  	#raise params.require(:visitor_besolution).inspect
     params.require(:user).permit(:username, :password)
   end
 end
