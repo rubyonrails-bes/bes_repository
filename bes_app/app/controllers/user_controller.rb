63fa034ef56e1221963b55e524bb4b1ecbf1e9a0
roopesh <roopesh.v1595@gmail.com>
2018-04-16 10:45:50 +0530

    roopesh new logins

--- bes_app/app/controllers/user_controller.rb
+++ bes_app/app/controllers/user_controller.rb
@@ -1,16 +1,32 @@
 class UserController < ApplicationController
+
+def login_user
+ #raise params[:user][:encrypted_password].inspect
+ @user = User.find(:all,:conditions, :username => params[:user][:username],:encrypted_password => params[:user][:encrypted_password])
+ 
+ if !@user.blank?
+  render :show
+else
+  redirect_to :root
+end
+end
+
+
+
 def new
 	@besapp = User.all
 end
 
   def create
+    
   	@besapp = User.new(bes_params)
   	  	
     if @besapp.save
+      llll
       
-      redirect_to :visitor_create_url, notice: 'Visitor was successfully saved.'
+      redirect_to :user_index, notice: 'Member was successfully saved.'
     else
-      render :show
+      render :index
     end
   end
   def show
@@ -18,7 +34,8 @@ end
        @visitor = User.find(params[:id])
 
   end
-
+def index
+end
 
   private
   def set_visitor
@@ -26,6 +43,7 @@ end
   end
 
   def bes_params
-    params.require(:user).permit(:username, :password)
+
+    params.permit(:username,:email, :encrypted_password)
   end
 end
--- bes_app/config/routes.rb
+++ bes_app/config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-get 'user/create'
+resources :user
+get "/user/create"
 get 'user/new'
-root "user#new"
+get 'user/index'
+get 'user/login_user'
+post 'user/login_user'
+
+root "user#index"
+
+
 end
